face1335d1f829ac87f0a640610efdf6b218bca8
Nneoma Chukwukelu <zitac234@gmail.com>
2021-06-18 06:17:16 -0400

    updated package json

---
+++ .history/client/index_20210618061432.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Table from './components/Table'
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  // <div>Hello, Nneoma Vivian Chukwukelu!</div>,
+  <Table/>, 
+  document.getElementById('app') 
+)
