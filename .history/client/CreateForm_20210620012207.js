92d3804f684d4f8fd8a07f9c3dd58c06a66c9f46
Nneoma Chukwukelu <zitac234@gmail.com>
2021-06-20 01:23:19 -0400

    can count sentences

---
+++ .history/client/CreateForm_20210620012207.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import Form  from './Form'
+
+export default class CreateForm extends React.Component{
+      constructor(){
+            super()
+            this.state = {
+                  search :'',
+                  text : '',
+            }
+            this.handleChange = this.handleChange.bind(this)
+            this.handleSubmit = this.handleSubmit.bind(this)
+            this.countSentence = this.countSentence.bind(this)
+      }
+      handleChange(evt){
+            this.setState({
+                  [evt.target.name]: evt.target.value
+            })
+      }
+      countSentence(){
+            let regex = /[!\.\?]/
+            let sentences = this.state.text.split(regex)
+            console.log('setences', sentences)
+            return sentences.filter(sentence => sentence.length > 0)
+            
+      }
+      async handleSubmit (evt){
+            evt.preventDefault()
+            let s = this.countSentence()
+            console.log('s' , s)
+            // console.log('state from submit',)
+      }
+      render(){
+            console.log('state', this.state)
+            return (
+                  <Form 
+                  {...this.state } 
+                  handleChange={this.handleChange} 
+                  handleSubmit={this.handleSubmit} 
+                  // countSentence={this.countSentence}
+                  />
+            )
+      }
+}
